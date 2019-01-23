a404534803219c5ac6a3c4695222a67fe534da20
masha <volkova.maria.d@gmail.com>
2019-01-23 12:13:37 +0300

    reformat fp

--- fp/test.hs
+++ fp/test.hs
@@ -22,32 +22,19 @@ unfoldr f = maybe [] (\ (a, b) -> a : unfoldr f b) . f
 
 
 -- splitAt n xs -- splitAt 3 [1..5] = ([1,2,3],[4,5])
-
 -- new_element : xs -- 5 : [1..3] = [5,1,2,3]
-
 -- map my_function xs -- map (+1) [1..3] = [2,3.4]
-
 -- drop n xs -- drop 2 [1..5] = [3,4,5]
-
 -- take n xs -- take 2 [1..5] = [1,2]
-
 -- lst !! n -- [0..10] !! 5 = 5
-
 -- filter foo xs -- filter (\x -> x>3) [1..5] = [4,5]
-
 -- elem el lst -- elem 58 [1..100] = True
-
 -- ls1 ++ ls2 -- [1..2] ++ [3,4] = [1,2,3,4]
-
 -- length lst -- length [0..10] = 11
-
 -- reverse lst -- reverse [1..3] = [3,2,1]
 
--- head, tail, minimum, maximum
 
-
--- Задания
--- объединить повторяющиеся элементы в списке
+-- Объединить повторяющиеся элементы в списке
 compress (h : t) = compress' t (h:[])
 compress' (h : t) lst = if (contain h lst == 1) then compress' t lst else compress' t (h : lst)
 compress' [] lst = lst
@@ -56,7 +43,7 @@ contain e (h : t) | e == h = 1
                   | e /= h = contain e t
 contain _ [] = 0
 
--- 1. Вар 2
+
 -- Поместить все последовательные повторяющиеся элементы списка в подсписки
 pack lst = unfoldr fun lst
 
@@ -69,10 +56,9 @@ makeSublist x (h : t) lst | h == x = makeSublist x t (x : lst)
 
 remove h (h' : t') lst | h == h' =  remove h t' lst
                        | h /= h' =  remove h t' (h' : lst)
-remove _ [] lst = lst    
+remove _ [] lst = lst
 
 
--- 2. Вар 2
 -- Разбить список на 2. Размер первого списка задан параметром
 mySplit :: [a] -> Int -> ([a], [a])
 mySplit list i = ((take i list), (drop i list))
@@ -82,18 +68,16 @@ mySplit' :: [a] -> Int -> ([a], [a])
 mySplit' lst n = splitAt n lst
 
 
--- список от до - только из составных чисел
+-- Список от до - только из составных чисел
 compList :: Integer -> Integer -> [Integer]
 compList from to = compList' from to []
 
 compList' :: Integer -> Integer -> [Integer] -> [Integer]
-compList' from to lst 
+compList' from to lst
     | from == to = lst
     | otherwise  = compList' (from +1) to foo where
     foo = if isPrime from then lst else lst ++ [from]
 
-
--- проверка на простоту
 isPrime :: Integer -> Bool
 isPrime n = isPrime' n 2
 
@@ -106,8 +90,6 @@ isPrime' x divider
     | otherwise          = isPrime' x (divider + 1)
 
 
-
--- 2. Вар 1
 -- Удалить каждый n-ый элемент из списка
 -- dropEvery :: [a] -> Int -> [a]
 -- dropEvery lst n = dropEvery' lst n (length div lst  n)
@@ -115,10 +97,8 @@ isPrime' x divider
 -- dropEvery' :: [a] -> Int -> Int -> [a]
 -- dropEvery' lst n m = if m < 0 then lst
 --     else (take (n-1) lst) ++ dropEvery' (drop n lst) n (m - 1)
---
---
 
--- 3. Вар 1
+
 -- Постройте всех спискок простых чисел в диапазоне
 primeList :: Integer -> Integer -> [Integer]
 primeList from to = primeList' from to []
@@ -128,20 +108,18 @@ primeList' from to lst
     | from == to = lst
     | otherwise  = primeList' (from + 1) to foo where
     foo = if isPrime from then lst ++ [from] else lst
---
---
---
--- -- Найти сумму всех натуральных чисел от 1 до n
--- sumBetween :: Integer -> Integer
--- sumBetween n = if n > 0
---     then foldl (+) 0 [1..n]
---     else foldl (+) 0 [n..1]
---
---
--- -- Создать структуру из "true" "false" "unknown" и реализовать "и", "или" и "не равно"
---
--- data MyData = MyTrue | MyFalse | MyUnknown
---
+
+
+-- Найти сумму всех натуральных чисел от 1 до n
+sumBetween :: Integer -> Integer
+sumBetween n = if n > 0
+    then foldl (+) 0 [1..n]
+    else foldl (+) 0 [n..1]
+
+
+-- Создать структуру из "true" "false" "unknown" и реализовать "и", "или" и "не равно"
+data NewData = MyTrue | MyFalse | MyUnknown
+
 -- (||) l r = case (l,r) of
 --     (MyTrue, _) -> MyTrue
 --     (_, MyTrue) -> MyTrue
@@ -160,46 +138,42 @@ primeList' from to lst
 --     (MyTrue, MyFalse) -> MyTrue
 --     (MyFalse, MyTrue) -> MyTrue
 --     otherwise -> MyUnknown
---
---
---
--- -- еще задача
--- data MyData = Plus Integer | Minus Integer | Equal
---
--- fun12 lst = fun13 lst []
--- -- fun12 (h:[])  = l
--- fun13 (h:t) lst = fun14 h (take 1 t) : lst
--- fun14 x y
+
+
+-- Создать список вида Plus x, Minus x, Equal входной последовательности
+data MyData = Plus Integer | Minus Integer | Equal
+
+-- myDataFun lst = myDataFun2 lst []
+-- myDataFun (h:[])  = l
+-- myDataFun2 (h:t) lst = myDataFun3 h (take 1 t) : lst
+-- myDataFun3 x y
 --     | a > 0 = Plus a
 --     | a < 0 = Minus a
 --     | a == 0 = Equal
 --     where a = x - y
---
---
--- -- еще задача
--- data MyData2 = Multiple Char Integer | Single Char deriving(Show)
---
--- fuck (h:t) = fff h t 1
---
--- fff x (h:t) acc
---     | h == x = fff x t (acc + 1)
---     | h /= x && acc > 1 = Multiple x acc : fff h t 1
---     | h /= x && acc == 1 = Single x : fff h t 1
---
--- fff x [] acc
---     | acc>0 = Multiple x acc : []
---     | acc == 0 = Single x : []
---
 
--- функция, кот сдвигает все элементы по кругу
-rotate2 lst y = if (y > 0) then let (a,b) = splitAt (length lst - y) lst in b ++ a else let (a,b) = splitAt (-y) lst in b ++ a
 
+-- Создать список вида Multiple a x, Single a входной последовательности
+data MyData2 = Multiple Char Integer | Single Char deriving(Show)
+
+myData2Fun (h:t) = fff h t 1
+
+fff x (h:t) acc
+    | h == x = fff x t (acc + 1)
+    | h /= x && acc > 1 = Multiple x acc : fff h t 1
+    | h /= x && acc == 1 = Single x : fff h t 1
+
+fff x [] acc
+    | acc>0 = Multiple x acc : []
+    | acc == 0 = Single x : []
+
+
+-- Функция, кот сдвигает все элементы по кругу
 rotate3 lst y = let (a,b) = if (y > 0) then splitAt (length lst - fuck (length lst) y) lst  else splitAt (fuck (length lst) (abs y)) lst in  b ++ a
 fuck len num  = if (num > len) then fuck len (num - len)  else num
 
 
-
--- найти на бесконечном списке два числа дающих в сумме заданное число
+-- Найти на бесконечном списке два числа дающих в сумме заданное число
 myFuncNumFuck y (h:t) = myFuncNumFuck' y t [h]
 
 myFuncNumFuck' y (h : t) r =  if (hasSum y h r) then takeSum y h r
