b55c8d313c576c071546458ea38993cd6e1da3e8
stakenschneider <volkova.maria.d@gmail.com>
2020-10-21 00:56:40 +0300

    fix fp test

--- 1/fp/Test_3.hs
+++ 1/fp/Test_3.hs
@@ -1,20 +1,23 @@
 module Test where
 
 -- Создать структуру из "true" "false" "unknown" и реализовать "и", "или" и "не равно"
-data NewData = MyTrue | MyFalse | MyUnknown
+data MyBool = MyTrue | MyFalse | MyUnknown
 
+(||) :: MyBool -> MyBool -> MyBool
 (||) l r = case (l,r) of
     (MyTrue, _) -> MyTrue
     (_, MyTrue) -> MyTrue
     (MyFalse, MyFalse) -> MyFalse
     otherwise -> MyUnknown
 
+(&) :: MyBool -> MyBool -> MyBool
 (&) l r = case (l,r) of
     (MyTrue, MyTrue) -> MyTrue
     (MyFalse, _) -> MyFalse
     (_, MyFalse) -> MyFalse
     otherwise -> MyUnknown
 
+(/=) :: MyBool -> MyBool -> MyBool
 (/=) l r = case (l,r) of
     (MyTrue, MyTrue) -> MyFalse
     (MyFalse, MyFalse) -> MyFalse
@@ -24,46 +27,45 @@ data NewData = MyTrue | MyFalse | MyUnknown
 
 
 -- Создать список вида Plus x, Minus x, Equal входной последовательности
-data MyData = Plus Integer | Minus Integer | Equal
-
-myDataFun lst = myDataFun2 lst []
-myDataFun (h:[])  = l
-myDataFun2 (h:t) lst = myDataFun3 h (take 1 t) : lst
-myDataFun3 x y
-    | a > 0 = Plus a
-    | a < 0 = Minus a
-    | a == 0 = Equal
-    where a = x - y
+data PlusMinusEq = Plus Integer | Minus Integer | Equal deriving (Show)
+fun :: [Integer] -> [PlusMinusEq]
+fun (h:[]) = []
+fun (h:t) | a > 0 = Plus a : fun t
+          | a < 0 = Minus (-a) : fun t
+          | a == 0 = Equal : fun t
+          where a = head t - h
 
 
--- Создать список вида Multiple a x, Single a входной последовательности
-data MyData2 = Multiple Char Integer | Single Char deriving(Show)
+-- Два списка (одинаковой длинны), написать функцию разницы.
+data Difference = Same | Different Int Int deriving (Show)
 
-myData2Fun (h:t) = fff h t 1
-
-fff x (h:t) acc
-    | h == x = fff x t (acc + 1)
-    | h /= x && acc > 1 = Multiple x acc : fff h t 1
-    | h /= x && acc == 1 = Single x : fff h t 1
-
-fff x [] acc
-    | acc>0 = Multiple x acc : []
-    | acc == 0 = Single x : []
+diff :: [Int] -> [Int] -> [Difference]
+diff (h:t) (h1:t1) | h == h1   = Same : diff t t1
+                   | otherwise = Different h h1 : diff t t1
+diff [] _ = []
+diff _ [] = []
 
 
--- Два списка, написать функцию разницы
-data MyData3 = Same | Different Int Int deriving(Show)
-
-diff (h:t)(h1:t1) = if (h == h1) then Same : diff t t1 else Different h h1 : diff t t1
-diff [] = []
-
-
---Реализуйте тип данных, позволяющий задавать команды управления лентой вида "Влево на определённое число шагов",
+-- Реализуйте тип данных, позволяющий задавать команды управления лентой вида "Влево на определённое число шагов",
 -- "Вправо на определённое число шагов", "Запись". Реализуйте функцию, которая принимает на вход список
 -- команд и начальную позицию и возвращает список позиций, на которых происходила запись.
-data MyData = MLeft Int | MRight Int | MWrite;
+data Conveyor = MLeft Int | MRight Int | MWrite;
+
+foo :: [Conveyor] -> Int -> [Int]
 foo [] x = []
 foo (h:t) x = case h of
                   MLeft y -> foo t (x-y)
                   MRight y -> foo t (x+y)
                   MWrite -> x : (foo t x)
+
+
+-- Создать список вида Multiple a x, Single a входной последовательности
+data SingleOrMultiple = Multiple Char Integer | Single Char deriving (Show)
+
+function :: [Char] -> [SingleOrMultiple]
+function (h:t) = f h t 1
+f x [] acc = if acc == 1 then Single x : [] else Multiple x acc : []
+f x (h:t) acc | h == x = f h t (acc + 1)
+              | acc == 1 = Single x : f h t 1
+              | acc > 1 = (Multiple x acc) : f h t 1
+
